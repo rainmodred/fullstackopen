2a6a069a1721564ea0b9d84a2e85e158a57cd133
rainmodred <albphoenix@gmail.com>
2019-10-06 17:03:17 +0300

    step 1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Header({ course }) {
+  return <h1>{course}</h1>;
+}
+
+function Content({ content }) {
+  return (
+    <>
+      {content.map(({ part, exercisesCount }) => {
+        return (
+          <p>
+            {part} {exercisesCount}
+          </p>
+        );
+      })}
+    </>
+  );
+}
+
+function Total({ total }) {
+  return <p>Number of exercises {total}</p>;
+}
+
+function App() {
+  const course = 'Half Stack application development';
+  const content = [
+    { part: 'Fundamentals of React', exercisesCount: 10 },
+    { part: 'Using props to pass data', exercisesCount: 7 },
+    { part: 'State of a component', exercisesCount: 14 },
+  ];
+
+  const total = content.reduce((prev, curr, indx, array) => {
+    return prev + curr.exercisesCount;
+  }, 0);
+
+  return (
+    <>
+      <Header course={course} />
+      <Content content={content} />
+      <Total total={total} />
+    </>
+  );
+}
+
+export default App;
