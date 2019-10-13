8a81a93b0010f21f185894cace1e1bb678fa7cfe
rainmodred <albphoenix@gmail.com>
2019-10-13 17:33:22 +0300

    part3/phonebook: step 1

---
+++ part3/backend/index.js
@@ -0,0 +1,39 @@
+const app = require('express')();
+
+let notes = [
+  {
+    name: 'Arto Hellas',
+    number: '041-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
+];
+
+app.get('/api/persons/', (request, response) => {
+  response.json(notes);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find(note => note.id === id);
+  response.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
