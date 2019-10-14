541352fa25ef40e3ac0db99c0e1c0468db92e858
rainmodred <albphoenix@gmail.com>
2019-10-14 20:21:40 +0300

    part3/phonebook: step 11

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,12 +1,14 @@
-const app = require('express')();
+const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 morgan.token('body', req => JSON.stringify(req.body));
 app.use(morgan(`:method :url :status :res[content-length] - :response-time ms :body`));
+app.use(express.static('build'));
 
 let persons = [
   {
