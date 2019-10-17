e6234ac7a1576f91f46642db35f0eb69ca7bcb20
rainmodred <albphoenix@gmail.com>
2019-10-17 19:52:29 +0300

    part3/phonebook database: step 3

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 const app = express();
 app.use(bodyParser.json());
@@ -46,13 +45,6 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  // const checkPerson = persons.find(({ name }) => name.toLowerCase() === body.name.toLowerCase());
-  // if (checkPerson) {
-  //   return response.status(409).json({
-  //     error: 'name must be unique',
-  //   });
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -67,27 +59,51 @@ app.post('/api/persons', (request, response) => {
 
 app.get('/api/persons/', (request, response) => {
   Person.find({}).then(persons => {
-    console.log(`persons ${persons}`);
     response.json(persons.map(person => person.toJSON()));
   });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person.toJSON());
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
