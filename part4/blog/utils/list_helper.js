79c68793b5baff758bc60abeb43bf4161eba7ead
rainmodred <albphoenix@gmail.com>
2019-10-20 23:42:03 +0300

    part4/blog: step 3

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,14 @@ function totalLikes(blogs) {
   return blogs.reduce((prev, curr) => prev + curr.likes, 0);
 }
 
+function favoriteBlog(blogs) {
+  const maxLikes = Math.max(...blogs.map(({ likes }) => likes));
+
+  return blogs.find(({ likes }) => likes === maxLikes);
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
