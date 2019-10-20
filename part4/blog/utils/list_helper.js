78a7a75d4916c4115df991b95ecb0c94f2b5dce0
rainmodred <albphoenix@gmail.com>
2019-10-20 23:18:35 +0300

    part4/blog: step2

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ function dummy(blogs) {
   return 1;
 }
 
+function totalLikes(blogs) {
+  return blogs.reduce((prev, curr) => prev + curr.likes, 0);
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 };
