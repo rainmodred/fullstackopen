853bf2c08ee547c6d217ae5d679992a5e54892c5
rainmodred <albphoenix@gmail.com>
2019-10-27 21:02:16 +0300

    part5: 5.6

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -4,14 +4,15 @@ import Notification from './Notification';
 
 export default function Blogs({ blogs, notification }) {
   if (blogs.length === 0) return null;
+
   return (
     <>
       <h2>blogs</h2>
       {notification !== null && (
         <Notification message={notification.message} type={notification.type} />
       )}
-      {blogs.map(({ title, author, id }) => (
-        <Blog key={id} title={title} author={author} />
+      {blogs.map(({ title, author, id, user, url, likes }) => (
+        <Blog key={id} title={title} author={author} likes={likes} url={url} user={user} />
       ))}
     </>
   );
