9cfbdc9bd7785a1626d3d3def8c79792ece3450d
rainmodred <albphoenix@gmail.com>
2019-10-27 21:02:16 +0300

    part5: 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Blog({ title, author }) {
+function Blog({ title, author, url, likes, user }) {
+  console.log({ title, author, url, likes, user });
+  const [toggled, setToggled] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (toggled)
+    return (
+      <div style={blogStyle}>
+        <div onClick={() => setToggled(!toggled)}>
+          {title} {author}
+        </div>
+        <div>
+          <a href={url}> {url}</a>
+        </div>
+        <div>
+          {likes} likes <button>like</button>
+        </div>
+        <div>added by {user.name}</div>
+      </div>
+    );
+
   return (
-    <div>
-      {title} {author}
+    <div style={blogStyle}>
+      <div onClick={() => setToggled(!toggled)}>
+        {title} {author}
+      </div>
     </div>
   );
 }
