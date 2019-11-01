af294fa9b46e33d96f3845df01a374d3428bacc4
rainmodred <albphoenix@gmail.com>
2019-11-01 21:26:59 +0300

    part6: 6.15

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,39 +3,40 @@ import { connect } from 'react-redux';
 import { incVote } from '../reducers/anecdoteReducer';
 import { showNotification } from '../reducers/notificationReducer';
 
-const AnecdoteList = ({ anecdotes, filter, incVote, showNotification }) => {
+const AnecdoteList = ({ visibleAnecdotes, incVote, showNotification }) => {
   const vote = id => {
     incVote(id);
 
-    showNotification(`you voted '${anecdotes.find(anecdote => anecdote.id === id).content}'`);
+    showNotification(
+      `you voted '${visibleAnecdotes.find(anecdote => anecdote.id === id).content}'`,
+    );
     setTimeout(() => {
       showNotification(``);
     }, 5000);
   };
 
-  const anecdotesToShow = () => {
-    return anecdotes
-      .filter(({ content }) => content.toLowerCase().includes(filter.toLowerCase()))
-      .sort((a, b) => b.votes - a.votes)
-      .map(anecdote => {
-        return (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
+  return visibleAnecdotes
+    .sort((a, b) => b.votes - a.votes)
+    .map(anecdote => {
+      return (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
-        );
-      });
-  };
-  return anecdotesToShow();
+        </div>
+      );
+    });
+};
+
+const anecdotesToShow = ({ anecdotes, filter }) => {
+  return anecdotes.filter(({ content }) => content.toLowerCase().includes(filter.toLowerCase()));
 };
 
 const mapStateToProps = state => {
   return {
-    anecdotes: state.anecdotes,
-    filter: state.filter,
+    visibleAnecdotes: anecdotesToShow(state),
   };
 };
 
