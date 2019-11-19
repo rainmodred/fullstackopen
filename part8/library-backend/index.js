75cb610b82656c39c2d5b2dfe6038ecce06e7d65
rainmodred <albphoenix@gmail.com>
2019-11-19 19:04:05 +0300

    part8: remove comments

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -22,14 +22,6 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     console.log('error connection to MongoDB:', error.message);
   });
 
-
-/*
- * It would be more sensible to assosiate book and the author by saving
- * the author id instead of the name to the book.
- * For simplicity we however save the author name.
- */
-
-
 const typeDefs = gql`
 
   type Author {
