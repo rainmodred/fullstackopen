883e7a2496925508e0ff5aae181aa43d4ca2ae99
rainmodred <albphoenix@gmail.com>
2020-04-15 15:09:34 +0300

    9.5

--- part9/ts-server/bmiCalculator.ts
+++ part9/ts-server/bmiCalculator.ts
@@ -1,34 +1,36 @@
-interface BmiValues {
-  height: number;
-  weight: number;
-}
+// interface BmiValues {
+//   height: number;
+//   weight: number;
+// }
 
-function parseArguments(args: Array<string>): BmiValues {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+// function parseBmiArgs(args: Array<string>): BmiValues {
+//   if (args.length < 4) throw new Error('Not enough arguments');
+//   if (args.length > 4) throw new Error('Too many arguments');
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3]),
-    };
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
+//   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//     return {
+//       height: Number(args[2]),
+//       weight: Number(args[3]),
+//     };
+//   } else {
+//     throw new Error('Provided values were not numbers!');
+//   }
+// }
 
-function calculateBmi(height: number, weight: number): string {
+export function calculateBmi(height: number, weight: number): string {
   const BMI = weight / (height / 100) ** 2;
 
   if (BMI < 18.5) return 'Underweight';
   if (BMI >= 18.5 && BMI <= 25) return 'Normal (healthy weight)';
   if (BMI > 25 && BMI <= 30) return 'Overweight ';
   if (BMI > 30) return 'Obese';
+
+  throw new Error('Invalid values');
 }
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
-}
+// try {
+//   const { height, weight } = parseBmiArgs(process.argv);
+//   console.log(calculateBmi(height, weight));
+// } catch (e) {
+//   console.log('Error, something bad happened, message: ', e.message);
+// }
--- part9/ts-server/index.ts
+++ part9/ts-server/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  const { height, weight } = req.query;
+
+  if (!isNaN(parseInt(height)) && !isNaN(parseInt(weight))) {
+    const bmi = calculateBmi(parseInt(height), parseInt(weight));
+    res.json({ weight, height, bmi });
+  } else {
+    res.status(400).send({
+      error: 'malformatted parameters',
+    });
+  }
 });
 
 const PORT = 3000;
