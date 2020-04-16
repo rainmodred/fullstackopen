947d47c8809eee75a7a848798808354f692db825
rainmodred <albphoenix@gmail.com>
2020-04-16 17:33:39 +0300

    9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
+import diagnosesRouter from './routes/diagnosesRouter';
 const app = express();
 
 app.use(express.json());
@@ -12,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,13 @@
+import diagnosesData from '../../data/diagnoses';
+
+import { DiagnoseEntry } from '../types';
+
+const diagnoses: Array<DiagnoseEntry> = diagnosesData;
+
+const getEntries = (): Array<DiagnoseEntry> => {
+  return diagnoses;
+};
+
+export default {
+  getEntries,
+};
